45813a75b22372081e853a7b064116ac0e171bd5
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-08-20 19:08:52 +0530

    Create Binary-Tree-Nodes.sql

---
+++ SQL/Advanced Select/Binary-Tree-Nodes.sql
@@ -0,0 +1,3 @@
+Problem Statement : https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+
+SELECT N, IF(P IS NULL,'Root',IF(B.N IN (SELECT P FROM BST),'Inner','Leaf')) FROM BST AS B ORDER BY N ASC;
