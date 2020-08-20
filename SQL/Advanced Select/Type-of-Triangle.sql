ddee58ba9d2c4ada23b6280121d3cd1c64278541
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-08-20 19:14:53 +0530

    Create Type-of-Triangle.sql

---
+++ SQL/Advanced Select/Type-of-Triangle.sql
@@ -0,0 +1,3 @@
+Problem Statement : https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+SELECT IF(A+B>C AND A+C>B AND B+C>A, IF(A=B AND B=C,'Equilateral',IF(A=B OR B=C OR A=C,'Isosceles','Scalene')),'Not A Triangle') FROM TRIANGLES;
