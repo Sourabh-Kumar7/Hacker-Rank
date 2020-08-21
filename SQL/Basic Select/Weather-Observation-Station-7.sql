5ce58661350606f1524f4afade9290c835231c9f
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-08-21 14:03:04 +0530

    Update Weather-Observation-Station-7.sql

--- SQL/Basic Select/Weather-Observation-Station-7.sql
+++ SQL/Basic Select/Weather-Observation-Station-7.sql
@@ -1,3 +1,3 @@
-Problem Statement : https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+-- Problem Statement : https://www.hackerrank.com/challenges/weather-observation-station-7/problem
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '[a,e,i,o,u]$';
