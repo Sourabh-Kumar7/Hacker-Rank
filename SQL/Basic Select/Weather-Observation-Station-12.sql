14e2cf9db27fe81c85e8c5687d88a9ff2e891669
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-08-22 09:12:16 +0530

    Create Weather-Observation-Station-12.sql

---
+++ SQL/Basic Select/Weather-Observation-Station-12.sql
@@ -0,0 +1,3 @@
+-- Problem Statment : https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+
+SELECT DISTINCT CITY FROM STATION WHERE (CITY REGEXP '^[^AEIOU]' AND CITY REGEXP '[^AEIOU]$');
