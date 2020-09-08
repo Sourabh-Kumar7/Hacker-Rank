0ef3d56b1851be244efd08e415ee2f524cca226f
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-09-08 16:12:48 +0530

    Create Draw-The-Triangle-2.sql

---
+++ SQL/Alternative Queries/Draw-The-Triangle-2.sql
@@ -0,0 +1,4 @@
+-- Problem Statement : https://www.hackerrank.com/challenges/draw-the-triangle-2/problem
+
+SET @NUM = 0;
+SELECT REPEAT("* ",@NUM := @NUM +1) FROM INFORMATION_SCHEMA.TABLES WHERE @NUM<20;
