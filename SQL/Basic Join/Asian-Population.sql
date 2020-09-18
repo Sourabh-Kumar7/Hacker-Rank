c46e86dfb01035d4423ca555e7d4eaa51460504d
Sourabh Kumar <65007658+beyourwild7@users.noreply.github.com>
2020-09-18 14:24:12 +0530

    Create Asian-Population.sql

---
+++ SQL/Basic Join/Asian-Population.sql
@@ -0,0 +1,3 @@
+-- Problem Statement : https://www.hackerrank.com/challenges/asian-population/problem
+
+SELECT SUM(CITY.POPULATION) FROM CITY JOIN COUNTRY ON COUNTRY.CODE=CITY.COUNTRYCODE WHERE COUNTRY.CONTINENT = "Asia";
