9c70d61d03dbcd6914199949bf87e473b4ea0953
Sourabh Kumar <beyourwild7@gmail.com>
2020-10-07 12:57:52 +0530

    Update Binary-Tree-Nodes.sql

--- SQL/Advanced Select/Binary-Tree-Nodes.sql
+++ SQL/Advanced Select/Binary-Tree-Nodes.sql
@@ -1,3 +1,5 @@
 -- Problem Statement : https://www.hackerrank.com/challenges/binary-search-tree-1/problem
 
-SELECT N, IF(P IS NULL,'Root',IF(B.N IN (SELECT P FROM BST),'Inner','Leaf')) FROM BST AS B ORDER BY N ASC;
+SELECT N, IF(P IS NULL,'Root',IF(B.N IN (SELECT P FROM BST),'Inner','Leaf')) 
+  FROM BST AS B 
+  ORDER BY N ASC;
