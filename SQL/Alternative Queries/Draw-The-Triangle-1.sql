5438acd6201e85baca54738cec182521e2414bb5
Sourabh Kumar <beyourwild7@gmail.com>
2020-10-07 12:58:23 +0530

    Update Draw-The-Triangle-1.sql

--- SQL/Alternative Queries/Draw-The-Triangle-1.sql
+++ SQL/Alternative Queries/Draw-The-Triangle-1.sql
@@ -1,4 +1,6 @@
 -- Problem Statement : https://www.hackerrank.com/challenges/draw-the-triangle-1/problem
 
 SET @NUM = 21;
-SELECT REPEAT("* ",@NUM := @NUM - 1) FROM INFORMATION_SCHEMA.TABLES WHERE @NUM>0;
+SELECT REPEAT("* ",@NUM := @NUM - 1) 
+  FROM INFORMATION_SCHEMA.TABLES 
+  WHERE @NUM>0;
