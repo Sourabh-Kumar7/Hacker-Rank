8483e6aa1ce65af311b7944f44a28682ab109d22
Sourabh Kumar <beyourwild7@gmail.com>
2020-10-13 22:19:36 +0530

    Update Top-Earners.sql

--- SQL/Aggregation/Top-Earners.sql
+++ SQL/Aggregation/Top-Earners.sql
@@ -1,3 +1,6 @@
 -- Problem Statement : https://www.hackerrank.com/challenges/earnings-of-employees/problem
 
-SELECT (MONTHS*SALARY)AS EARNING, COUNT(*) FROM EMPLOYEE GROUP BY 1 ORDER BY EARNING DESC LIMIT 1;
+SELECT (MONTHS*SALARY)AS EARNING, COUNT(*) 
+  FROM EMPLOYEE 
+  GROUP BY 1 
+  ORDER BY EARNING DESC LIMIT 1;
