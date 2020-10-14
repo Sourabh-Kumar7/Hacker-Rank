a32be448002d5c95a39d8ab70f780fe3a15fad4f
Sourabh Kumar <beyourwild7@gmail.com>
2020-10-14 20:17:26 +0530

    Update Weather-Observation-Station-14.sql

--- SQL/Aggregation/Weather-Observation-Station-14.sql
+++ SQL/Aggregation/Weather-Observation-Station-14.sql
@@ -1,3 +1,6 @@
 -- Problem Statement : https://www.hackerrank.com/challenges/weather-observation-station-14/problem
 
-SELECT MAX(ROUND(LAT_N,4)) FROM STATION WHERE LAT_N<137.2345 ORDER BY LAT_N DESC;
+SELECT MAX(ROUND(LAT_N,4)) 
+  FROM STATION 
+  WHERE LAT_N<137.2345 
+  ORDER BY LAT_N DESC;
